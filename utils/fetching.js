9d713d5fcd33e213236ff5b70fb625c89f0159df
Alex Dieulot <adieulot@gmail.com>
2016-05-30 23:56:04 +0200

    Refactor fetching into one function

--- utils/fetching.js
+++ utils/fetching.js
@@ -1,10 +1,10 @@
 let http = require('http')
 , config = require('../config')
 
-function topic(mode, forumId, idlegacyOrModern, page, slug, successCallback, failCallback) {
+function fetch(url, successCallback, failCallback) {
   let request = http.request({
     hostname: 'www.jeuxvideo.com',
-    path: `/forums/${mode}-${forumId}-${idlegacyOrModern}-${page}-0-1-0-${slug}.htm`,
+    path: url,
     headers: {
       'Cookie': 'coniunctio=cache_bypass'
     }
@@ -27,30 +27,12 @@ function topic(mode, forumId, idlegacyOrModern, page, slug, successCallback, fai
   request.end()
 }
 
+function topic(mode, forumId, idlegacyOrModern, page, slug, successCallback, failCallback) {
+  fetch(`/forums/${mode}-${forumId}-${idlegacyOrModern}-${page}-0-1-0-${slug}.htm`, successCallback, failCallback)
+}
+
 function forum(id, slug, page, successCallback, failCallback) {
-  let request = http.request({
-    hostname: 'www.jeuxvideo.com',
-    path: `/forums/0-${id}-0-${page}-0-1-0-${slug}.htm`,
-    headers: {
-      'Cookie': 'coniunctio=cache_bypass'
-    }
-  }, (res) => {
-    let body = ''
-    res.on('data', (chunk) => {
-      body += chunk
-    })
-    res.on('end', () => {
-      successCallback(res.headers, body)
-    })
-  })
-
-  request.on('error', failCallback)
-
-  request.setTimeout(config.timeout, () => {
-    failCallback('timeout')
-  })
-
-  request.end()
+  fetch(`/forums/0-${id}-0-${page}-0-1-0-${slug}.htm`, successCallback, failCallback)
 }
 
 module.exports = {
