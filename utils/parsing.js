5a73984cbcfa39106b1a93c8159e6bcdc4e3442c
kiwec <c.wolf@kiwec.net>
2016-05-31 21:12:23 +0200

    Topic parsing with cheerio

--- utils/parsing.js
+++ utils/parsing.js
@@ -2,71 +2,63 @@ let cheerio = require('cheerio')
   , utils = require('./utils')
 
 function topic(body) {
-  let retour = {}
+  let $ = cheerio.load(body)
+    , r = {}
     , matches
     , regex
 
-  retour.title = false
-  if (matches = body.match(/<span id="bloc-title-forum">(.+)<\/span>/)) {
-    retour.title = matches[1]
-  }
+  r.title = $('#bloc-title-forum').text() || false
 
-  retour.forumSlug = retour.forumName = false
-  if (matches = body.match(/<span><a href="\/forums\/0-(?:[0-9]+)-0-1-0-1-0-([a-zA-Z0-9-]+)\.htm">Forum ([^<]+)<\/a><\/span>/)) {
-    retour.forumSlug = matches[1]
-    retour.forumName = matches[2]
-  }
+  let $forum = $('.fil-ariane-crumb > span').eq(2)
+  r.forumSlug = $forum.attr('href')
+  r.forumName = $forum.last().text().substring('Forum '.length)
+  r.messages = []
 
-  retour.messages = []
-  regex = /<div class="bloc-message-forum " data-id="([0-9]+)">\s+<div class="conteneur-message">\s+(?:<div class="bloc-avatar-msg">\s+<div class="back-img-msg">\s+<div>\s+<span[^>]+>\s+<img src="[^"]+" data-srcset="([^"]+)"[^>]+>\s+<\/span>\s+<\/div>\s+<\/div>\s+<\/div>\s+)?<div class="inner-head-content">[\s\S]+?(?:<span class="JvCare [0-9A-F]+ bloc-pseudo-msg text-([^"]+)"|<div class="bloc-pseudo-msg")[^>]+>\s+([\s\S]+?)\s+<[\s\S]+?<div class="bloc-date-msg">\s+(?:<span[^>]+>)?([0-9][\s\S]+?)(?:<\/span>)?\s+<\/div>[\s\S]+?<div class="txt-msg  text-enrichi-forum ">([\s\S]+?)<\/div><\/div>\s+<\/div>\s+<\/div>\s+<\/div>/g
-  while (matches = regex.exec(body)) {
-    let isNicknameDeleted = matches[4].includes('Pseudo supprimé')
-    retour.messages.push({
-      id: matches[1],
-      avatar: isNicknameDeleted || matches[2].includes('/default.jpg') ? false : matches[2],
-      status: matches[3],
-      nickname: matches[4],
+  $('.bloc-message-forum').each(function() {
+    let msg = $(this)
+    let img = msg.find('.back-img-msg > div > span > img'),
+        avatar = img.attr('data-srcset'),
+        nickname = msg.find('.bloc-pseudo-msg').text(),
+        isNicknameDeleted = (nickname == 'Pseudo supprimé')
+
+    if (isNicknameDeleted || avatar.includes('/default.jpg')) avatar = false
+
+    r.messages.push({
+      id: msg.attr('data-id'),
+      avatar,
+      status: img.attr('class').substring(0, img.attr('class').indexOf('-')),
+      nickname,
       isNicknameDeleted,
-      date: matches[5],
-      content: utils.adaptMessageContent(matches[6]),
+      date: msg.find('.bloc-date-msg').text(),
+      content: utils.adaptMessageContent(msg.find('.bloc-contenu').html())
     })
-  }
+  })
 
-  let page = false
-  regex = /<span class="page-active">([0-9]+)<\/span>/
-  if (matches = regex.exec(body)) {
-    page = parseInt(matches[1])
-  }
-
-  retour.lastPage = false
-  regex = /<span><a href="\/forums\/[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9a-z-]+\.htm" class="lien-jv">([0-9]+)<\/a><\/span>/g
-  while (matches = regex.exec(body)) {
-    retour.lastPage = parseInt(matches[1])
-  }
-
-  retour.paginationPages = []
+  let page = parseInt($('.page-active').last().text(), 10)
+  r.lastPage = parseInt($('.bloc-liste-num-page .lien-jv').eq(-2).text(), 10)
+  r.paginationPages = []
   if (page >= 5) {
-    retour.paginationPages.push(1)
+    r.paginationPages.push(1)
   }
   for (let i = Math.max(1, page - 3); i < page; i++) { // Previous three pages
-    retour.paginationPages.push(i)
+    r.paginationPages.push(i)
   }
-  retour.paginationPages.push(page)
-  if (page < retour.lastPage) {
-    for (let i = page + 1; i < Math.min(page + 4, retour.lastPage); i++) {
-      retour.paginationPages.push(i)
+  r.paginationPages.push(page)
+  if (page < r.lastPage) {
+    for (let i = page + 1; i < Math.min(page + 4, r.lastPage); i++) {
+      r.paginationPages.push(i)
     }
-    retour.paginationPages.push(retour.lastPage)
+    r.paginationPages.push(r.lastPage)
   }
 
-  retour.isLocked = retour.lockRationale = false
-  regex = /<div class="message-lock-topic">\s+Sujet fermé pour la raison suivante :\s+<span>([^<]+)<\/span>\s+<\/div>/
-  if (matches = regex.exec(body)) {
-    retour.isLocked = true
-    retour.lockRationale = matches[1]
+  let lock = $('.message-lock-topic')
+  r.isLocked = r.lockRationale = false
+  if (lock.length) {
+    r.isLocked = true
+    r.lockRationale = lock.text().substring(lock.text().indexOf(':') + 2)
   }
 
-  return retour
+  return r
 }
 
 function forum(body) {
