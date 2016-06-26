59f992183cb93fcbbd454e5ef238ba1e9c6cc562
Alex Dieulot <adieulot@gmail.com>
2016-06-27 00:26:47 +0200

    Stop timing login page render

--- routes/introduction.js
+++ routes/introduction.js
@@ -5,13 +5,9 @@ let express = require('express')
   , router = express.Router()
 
 router.get('/', (req, res, next) => {
-  let start = Date.now()
   res.render('introduction', {
     googleAnalyticsId: config.googleAnalyticsId,
     cacheBusting,
-  }, (err, html) => {
-    console.log((Date.now() - start) / 1000)
-    res.send(html)
   })
 })
 
