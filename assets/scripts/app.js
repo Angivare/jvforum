a46bb22cddaf82680b5f8e806b164f3b60fc423b
Alex Dieulot <adieulot@gmail.com>
2016-06-30 12:00:28 +0200

    Fix variable case

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -248,7 +248,7 @@ if (googleAnalyticsId) {
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', googleAnalyticsID, 'auto');
+  ga('create', googleAnalyticsId, 'auto');
 
   instantClick.on('change', function() {
     ga('set', 'dimension1', 'Member')
