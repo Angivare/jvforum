29c0a46b5aa6428c9ac8c2208ed1be7d0e79fab7
Alexande Dieulot <adieulot@gmail.com>
2016-11-18 23:26:38 +0100

    Rename assets/scripts/app.js to main.js

--- utils/prepareCacheBusting.js
+++ utils/prepareCacheBusting.js
@@ -12,7 +12,7 @@ content = content.replace(/url\(\/images\/([^.]+)\.([a-z]+)\)/g, (all, filename,
 let scriptsList = [
       'instantclick',
       'loading-indicator',
-      'app',
+      'main',
     ]
   , scripts = {}
 
