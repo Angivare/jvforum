eec4e661bf81efb8431dc1c7009515ba8089e0ea
Alexande Dieulot <adieulot@gmail.com>
2016-12-02 16:57:35 +0100

    fix unique SQL values collision in topics_visited_pages

--- routes/ajax.js
+++ routes/ajax.js
@@ -742,7 +742,7 @@ router.post('/topicPosition', (req, res, next) => {
 
   let {topicIdModern, messageId, answersCount} = req.body
 
-  db.query('INSERT INTO topics_visited_pages SET ?', {
+  db.query('INSERT IGNORE INTO topics_visited_pages SET ?', {
     userId: user.id,
     topicIdModern,
     page: 1 + Math.floor(answersCount / 20)
