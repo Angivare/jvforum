b68809e4a0ce21e1a441f3cf0effbc78213437c5
Alexande Dieulot <adieulot@gmail.com>
2016-12-04 14:21:56 +0100

    Add debug info on unsupported page

--- routes/unsupported.js
+++ routes/unsupported.js
@@ -6,6 +6,7 @@ let express = require('express')
 router.get('/unsupported', (req, res, next) => {
   res.render('unsupported', {
     googleAnalyticsId: config.googleAnalyticsId,
+    userAgent: req.headers['user-agent'],
   })
 })
 
