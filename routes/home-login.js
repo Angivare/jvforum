1b1d5c7f8503c3def634c3e98931ca2a2f80b02c
Alexande Dieulot <adieulot@gmail.com>
2017-03-07 22:47:12 +0100

    Link to official topic on home page

--- routes/home-login.js
+++ routes/home-login.js
@@ -2,6 +2,7 @@ let express = require('express')
   , cacheBusting = require('../utils/prepareCacheBusting')
   , utils = require('../utils/utils')
   , renderView = require('../utils/renderView')
+  , db = require('../utils/db')
   , config = require('../config')
   , router = express.Router()
 
@@ -10,16 +11,23 @@ router.get('/', (req, res, next) => {
 
   if (user) {
     utils.getUserFavorites(user.id, (favorites) => {
-      res.send(renderView('home', {
-        userAgent: req.headers['user-agent'],
-        googleAnalyticsId: config.googleAnalyticsId,
-        timeouts: config.timeouts.client,
-        refreshIntervals: config.refreshIntervals,
-        cacheBusting,
-        title: 'JVForum',
-        favorites,
-        csrf: req.csrfToken(),
-      }))
+      db.select('numberOfPages', 'topics', {idModern: 39674315}, (results) => {
+        let jvfOfficialTopicNumberOfPages = 1
+        if (results.length) {
+          jvfOfficialTopicNumberOfPages = results[0].numberOfPages
+        }
+        res.send(renderView('home', {
+          userAgent: req.headers['user-agent'],
+          googleAnalyticsId: config.googleAnalyticsId,
+          timeouts: config.timeouts.client,
+          refreshIntervals: config.refreshIntervals,
+          cacheBusting,
+          title: 'JVForum',
+          favorites,
+          jvfOfficialTopicNumberOfPages,
+          csrf: req.csrfToken(),
+        }))
+      })
     })
   }
   else {
