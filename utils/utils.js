95a98e7758c515dd08f64bff77229edca70effc0
Alexande Dieulot <adieulot@gmail.com>
2017-03-15 15:41:23 +0100

    .link, :hover/:active brightness

--- utils/utils.js
+++ utils/utils.js
@@ -52,7 +52,7 @@ function adaptMessageContent(content, id, authorNickname, postDateRaw) {
   regex = /<span class="JvCare[^<]+>([^<]+)(?:<i><\/i><span>([^<]+)<\/span>([^<]+))?<\/span>/g
   while (matches = regex.exec(content)) {
     let url = matches.slice(1).join('').replace(/\$/g, '$$$$')
-      , replace = `<a href="${url}" target="_blank" title="${url}">${url}</a>`
+      , replace = `<a class="link" href="${url}" target="_blank" title="${url}">${url}</a>`
     content = content.replace(matches[0], replace)
     regex.lastIndex += -matches[0].length + replace.length
   }
@@ -64,16 +64,16 @@ function adaptMessageContent(content, id, authorNickname, postDateRaw) {
   // 1. Add mailto: at the start
   // 2. Normalize it by de-obfuscating it. Each character of a mail is randomly encoded in one of two HTML entities styles;
   //    this causes the message's checksum to change at every refresh, thus making it look as if the message has changed.
-  regex = /<a href="((?:&#[x0-9a-f]+;)+)"[^<]+>([^<]+)<\/a>/g
+  regex = /<a class="link" href="((?:&#[x0-9a-f]+;)+)"[^<]+>([^<]+)<\/a>/g
   while (matches = regex.exec(content)) {
     let email = entities.decode(matches[1]).replace(/\$/g, '$$$$')
-      , replace = `<a href="mailto:${email}" target="_blank" title="${email}">${email}</a>`
+      , replace = `<a class="link" href="mailto:${email}" target="_blank" title="${email}">${email}</a>`
     content = content.replace(matches[0], replace)
     regex.lastIndex += -matches[0].length + replace.length
   }
 
   // NoelShack thumbnails
-  content = content.replace(/<a href="(?:https?:)([^"]+)" data-def="NOELSHACK" target="_blank"><img class="img-shack" width="68" height="51" src="([^"]+)" alt="[^"]+"\/><\/a>/g, (all, href, src) => {
+  content = content.replace(/<a(?: class="[^"]+")? href="(?:https?:)([^"]+)" data-def="NOELSHACK" target="_blank"><img class="img-shack" width="68" height="51" src="([^"]+)" alt="[^"]+"\/><\/a>/g, (all, href, src) => {
     // Custom stickers
     if (src.indexOf('-jvforum-sticker') > -1) {
       let noelshackId = '/' + src.split('/').splice(4).join('/')
@@ -84,17 +84,17 @@ function adaptMessageContent(content, id, authorNickname, postDateRaw) {
       }
     }
 
-    return `<a class="noelshack-link" href="https:${href}" target="_blank" title="https:${href}"><img class="noelshack-link__thumb" src="https:${src}" alt="https:${href}"></a>`
+    return `<a class="noelshack-link primitive-element-state" href="https:${href}" target="_blank" title="https:${href}"><img class="noelshack-link__thumb" src="https:${src}" alt="https:${href}"></a>`
   })
 
   // Custom stickers on a single line, should be by themselves in order to be enlarged via CSS
   content = content.replace(/<p>\s*(<img class="js-sticker[^>]+>)\s*<\/p>/, "$1")
 
   // Make NoelShack links go directly to the image file
-  content = content.replace(/<a class="noelshack-link" href="(?:https:\/\/www\.noelshack\.com\/([0-9]+)-([0-9]+)-([^"]+))" target="_blank" title="[^"]+">/g, '<a class="noelshack-link" href="https://image.noelshack.com/fichiers/$1/$2/$3" target="_blank" title="https://image.noelshack.com/fichiers/$1/$2/$3">')
+  content = content.replace(/<a(?: class="[^"]+")? href="(?:https:\/\/www\.noelshack\.com\/([0-9]+)-([0-9]+)-([^"]+))" target="_blank" title="[^"]+">/g, '<a class="noelshack-link primitive-element-state" href="https://image.noelshack.com/fichiers/$1/$2/$3" target="_blank" title="https://image.noelshack.com/fichiers/$1/$2/$3">')
 
   // Correct thumbnails for PSD and SWF on NoelShack
-  content = content.replace(/(<a class="noelshack-link" href="[^"]+\.swf"[^<]+>)<img class="noelshack-link__thumb" src="[^"]+"/g, '$1<img class="noelshack-link__thumb" src="/assets/images/noelshack_swf_thumbnail.png"')
+  content = content.replace(/(<a(?: class="[^"]+")? href="[^"]+\.swf"[^<]+>)<img class="noelshack-link__thumb" src="[^"]+"/g, '$1<img class="noelshack-link__thumb" src="/assets/images/noelshack_swf_thumbnail.png"')
 
   // Spoils
   content = content.replace(/<span class="bloc-spoil-jv en-ligne">.+?<span class="contenu-spoil">/g, '<span class="spoil spoil--inline"><span class="spoil__content">')
@@ -104,7 +104,7 @@ function adaptMessageContent(content, id, authorNickname, postDateRaw) {
   content = content.replace(/<blockquote class="blockquote-jv">/g, '<blockquote class="quote">')
 
   // JVF links to topics and forums
-  content = content.replace(/<a href="(https?:\/\/(?:www|m)\.jeuxvideo\.com\/forums\/([0-9]+)-([0-9]+)-([0-9]+)-([0-9]+)-0-1-0-([0-9a-z-]+)\.htm(?:\#post_([0-9]+))?)"[^>]+>([^<]+)<\/a>/g, (all, url, mode, forumId, topicIdlegacyOrModern, page, slug, messageId, text) => {
+  content = content.replace(/<a(?: class="[^"]+")? href="(https?:\/\/(?:www|m)\.jeuxvideo\.com\/forums\/([0-9]+)-([0-9]+)-([0-9]+)-([0-9]+)-0-1-0-([0-9a-z-]+)\.htm(?:\#post_([0-9]+))?)"[^>]+>([^<]+)<\/a>/g, (all, url, mode, forumId, topicIdlegacyOrModern, page, slug, messageId, text) => {
     let path = '/' + forumId
     if ((mode == 1 || mode == 42) && forumId != 0) {
       path += '/'
@@ -122,11 +122,11 @@ function adaptMessageContent(content, id, authorNickname, postDateRaw) {
     if (messageId) {
       path += '#m' + messageId
     }
-    return `<a href="${path}" title="${url}" data-link-jvc="${url}">jvforum.fr${path}</a>`
+    return `<a class="link" href="${path}" title="${url}" data-link-jvc="${url}">jvforum.fr${path}</a>`
   })
 
   // Make all non-JVF links open in a new tab for those that normally don't (jeuxvideo.com, wearefans.com, allocine.fr, etc.)
-  content = content.replace(/<a href="([^/][^"]+)"( title|>)/g, '<a href="$1" target="_blank"$2')
+  content = content.replace(/<a(?: class="([^"]+)")? href="([^/][^"]+)"( title|>)/g, '<a class="$1" href="$2" target="_blank"$3')
 
   // Smileys
   content = content.replace(/<img src="\/\/image\.jeuxvideo\.com\/smileys_img\/([^.]+)\.(?:gif|png)" alt="([^"]+)" data-def="SMILEYS" data-code="[^"]+" title="[^"]+" \/>/g, '<img class="smiley smiley--$1" src="/assets/smileys/v1/$1" data-code="$2" title="$2" alt="$2">')
@@ -142,21 +142,21 @@ function adaptMessageContent(content, id, authorNickname, postDateRaw) {
   })
 
   // Show thumbnails for YouTube links
-  content = content.replace(/<a href="(https?:\/\/(?:[a-z]+\.)?youtube\.com\/watch[^"]*(?:\?|&amp;)v=([a-zA-Z0-9-_]{11})([^"])*)"[^>]+>[^<]+<\/a>/g, '<a class="youtube-link" href="$1" target="_blank" title="$1"><img class="youtube-link__thumb" src="https://img.youtube.com/vi/$2/mqdefault.jpg" alt="$1"></a>')
-  content = content.replace(/<a href="(https?:\/\/youtu\.be\/([a-zA-Z0-9-_]{11})([^"])*)"[^>]+>.+?<\/a>/g, '<a class="youtube-link" href="$1" target="_blank" title="$1"><img class="youtube-link__thumb" src="https://img.youtube.com/vi/$2/mqdefault.jpg" alt="$1"></a>')
+  content = content.replace(/<a(?: class="[^"]+")? href="(https?:\/\/(?:[a-z]+\.)?youtube\.com\/watch[^"]*(?:\?|&amp;)v=([a-zA-Z0-9-_]{11})([^"])*)"[^>]+>[^<]+<\/a>/g, '<a class="youtube-link" href="$1" target="_blank" title="$1"><img class="youtube-link__thumb" src="https://img.youtube.com/vi/$2/mqdefault.jpg" alt="$1"></a>')
+  content = content.replace(/<a(?: class="[^"]+")? href="(https?:\/\/youtu\.be\/([a-zA-Z0-9-_]{11})([^"])*)"[^>]+>.+?<\/a>/g, '<a class="youtube-link" href="$1" target="_blank" title="$1"><img class="youtube-link__thumb" src="https://img.youtube.com/vi/$2/mqdefault.jpg" alt="$1"></a>')
 
   // Remove JVC embeds
   content = content.replace(/<div class="player-contenu">\s+<div class="embed-responsive embed-responsive-16by9">\s+<div class="embed-responsive-item" >\s+<div class="player-jv" id="player-jv-[0-9]+-[0-9]+" data-srcset-video="[^"]+">Chargement du lecteur vidéo...<\/div>\s+<\/div>\s+<\/div>\s+<\/div>/g, '<p><small>[ Miniature vidéo de JVC, non-visible sur JVForum ]</small></p>')
   // Remove YouTube embeds
   content = content.replace(/<div class="player-contenu"><div class="embed-responsive embed-responsive-16by9"><iframe width="[0-9]+" height="[0-9]+" src="https:\/\/www\.youtube\.com\/embed\/([^"]+)\?feature=oembed" frameborder="0" allowfullscreen><\/iframe><\/div><\/div>/g, '<a class="youtube-link" href="https://youtu.be/$1" target="_blank" title="https://youtu.be/$1"><img class="youtube-link__thumb" src="https://img.youtube.com/vi/$1/mqdefault.jpg"></a>')
   // Remove Dailymotion embeds
-  content = content.replace(/<div class="player-contenu"><div class="embed-responsive embed-responsive-16by9"><iframe frameborder="0" width="[0-9]+" height="[0-9]+" src="https:\/\/www\.dailymotion\.com\/embed\/video\/([^"]+)" allowfullscreen><\/iframe><\/div><\/div>/g, '<a href="https://dai.ly/$1" target="_blank" title="https://dai.ly/$1">https://dai.ly/$1</a>')
+  content = content.replace(/<div class="player-contenu"><div class="embed-responsive embed-responsive-16by9"><iframe frameborder="0" width="[0-9]+" height="[0-9]+" src="https:\/\/www\.dailymotion\.com\/embed\/video\/([^"]+)" allowfullscreen><\/iframe><\/div><\/div>/g, '<a class="link" href="https://dai.ly/$1" target="_blank" title="https://dai.ly/$1">https://dai.ly/$1</a>')
   // Remove Twitch embeds
-  content = content.replace(/<div class="player-contenu">\s*<div class="embed-responsive embed-responsive-16by9">\s*<iframe src="https:\/\/player\.twitch\.tv\/\?channel=([^&]+)&autoplay=false" allowfullscreen><\/iframe>\s*<\/div>\s*<\/div>/g, '<a href="https://www.twitch.tv/$1" target="_blank" title="$1">https://www.twitch.tv/$1</a>')
+  content = content.replace(/<div class="player-contenu">\s*<div class="embed-responsive embed-responsive-16by9">\s*<iframe src="https:\/\/player\.twitch\.tv\/\?channel=([^&]+)&autoplay=false" allowfullscreen><\/iframe>\s*<\/div>\s*<\/div>/g, '<a class="link" href="https://www.twitch.tv/$1" target="_blank" title="$1">https://www.twitch.tv/$1</a>')
 
   // Cut long links
-  content = content.replace(/<a ([^>]+)>([^<]{90,})<\/a>/g, (all, attributes, text) => {
-    return `<a class="long-link" ${attributes}>` + text.substr(0, 85) + '<span class="long-link__hidden-part">' + text.substr(85) + '</span></a>'
+  content = content.replace(/<a(?: class="[^"]+")? ([^>]+)>([^<]{90,})<\/a>/g, (all, attributes, text) => {
+    return `<a class="link long-link" ${attributes}>` + text.substr(0, 85) + '<span class="long-link__hidden-part">' + text.substr(85) + '</span></a>'
   })
 
   content = emojify(content)
