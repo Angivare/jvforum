2c8f73f876fb901ee1379c6d529dc7491a76a286
Alexande Dieulot <adieulot@gmail.com>
2017-03-19 15:54:24 +0100

    Put back .sticker class on stage stickers/emoji

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -518,7 +518,7 @@ function enlargeEmoji() {
         <div class="stage-sticker-sd-container">
           <img class="stage-sticker-sd sticker sticker--pack-emoji" style="---width: 160px; ---height: 160px;" src="/assets/emoji/40/${image}">
         </div>
-        <img class="stage-sticker-hd sticker--pack-emoji" style="---width: 160px; ---height: 160px;" src="/assets/emoji/160/${image}">
+        <img class="stage-sticker-hd sticker sticker--pack-emoji" style="---width: 160px; ---height: 160px;" src="/assets/emoji/160/${image}">
       </div>
     </div>
   `
@@ -551,7 +551,7 @@ function enlargeSticker() {
         <div class="stage-sticker-sd-container">
           <img class="stage-sticker-sd sticker sticker--pack-${packId}" style="---width: 560px; ---height: 560px;" src="/assets/stickers/v2/${id}">
         </div>
-        <img class="stage-sticker-hd sticker--pack-${packId}" style="---width: 560px; ---height: 560px;" src="/assets/stickers/big/${id}">
+        <img class="stage-sticker-hd sticker sticker--pack-${packId}" style="---width: 560px; ---height: 560px;" src="/assets/stickers/big/${id}">
       </div>
       <div class="stage-sticker-container__code">
         <div class="stage-sticker-code">:${id}:</div>
